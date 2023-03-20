ef5d352037a50f3578cf9487839fb6df19d457cb
a-rosa <ameliarosalind@my.jcu.edu.au>
2023-03-20 22:45:16 +0800

    Add docstring to program

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -6,15 +6,18 @@ MINIMUM_LENGTH = 8
 
 
 def main():
+    """Main function is to get password and output password as stars"""
     password = get_password()
     print_password_to_stars(password)
 
 
 def print_password_to_stars(password):
+    """This function is to print stars with the same length as password"""
     print("Password:", "*" * len(password))
 
 
 def get_password():
+    """This function is to get valid password"""
     password = input("Enter password: ")
     while len(password) < MINIMUM_LENGTH:
         print("Minimum length not met")
