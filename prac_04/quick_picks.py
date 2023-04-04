b9773f76d942d0049a3abc156a1804ecf0c549f1
a-rosa <ameliarosalind@my.jcu.edu.au>
2023-04-04 13:50:41 +0800

    Add comments

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -6,12 +6,12 @@ AMOUNT_IN_A_PICK = 6
 quick_pick = int(input("How many quick picks: "))
 
 numbers = [number for number in range(MINIMUM_NUMBER, MAXIMUM_NUMBER + 1)]
-for counter in range(quick_pick):
+for counter in range(quick_pick): # Go through all the quick picks user inputs
     picked_numbers = []
-    for number_of_times in range(AMOUNT_IN_A_PICK):
-        random_index = random.randint(0, len(numbers)-1)
-        picked_numbers.append(numbers[random_index])
-        numbers.remove(numbers[random_index])
+    for number_of_times in range(AMOUNT_IN_A_PICK): # Loop six times to get 6 random numbers in one pick
+        random_index = random.randint(0, len(numbers)-1) # Get random index from the numbers list that has different length each time
+        picked_numbers.append(numbers[random_index]) # Append first the number in the index of the numbers list
+        numbers.remove(numbers[random_index]) # Delete that number from the numbers list, so the length changes
     picked_numbers = sorted(picked_numbers)
     for number in picked_numbers:
         print(f"{number:>2}", end=" ")
