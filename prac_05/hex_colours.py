e69f3161c907c114c97b21c40bd524258fdbacf2
a-rosa <ameliarosalind@my.jcu.edu.au>
2023-04-10 15:09:01 +0800

    Add prac 5 file

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,6 @@
+COLORS = {"Amber": "#ffbf00", "Beige": "#f5f5dc", "Chocolate": "#d2691e", "Emerald": "#50c878", "Gray": "#bebebe",
+          "Indigo": "#4b0082", "Lavender": "#e6e6fa", "Olive": "808000", "Purple": "#a020f0", "SkyBlue": "#87ceeb"}
+
+picked_color = input("Enter a color: ").upper()
+while picked_color != "":
+    if picked_color in
