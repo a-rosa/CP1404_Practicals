4a1642b30f1ede0d6310ada81adc6278a95c6963
a-rosa <ameliarosalind@my.jcu.edu.au>
2023-04-11 14:03:07 +0800

    Add comments

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -7,10 +7,11 @@ Actual: 40 mins
 text = input("Text: ").lower()
 words = text.split()
 word_occurences = {}
+# initialize the longest width to the width to the first word for comparison
 width = len(words[0])
 for word in words:
     if word in word_occurences:
-        word_occurences[word] += 1
+        word_occurences[word] += 1 # add one to the value of the key
     else:
         word_occurences[word] = 1
         if len(word) > width:
