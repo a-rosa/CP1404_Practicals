50ef75ac5a3f55829d68fcba3dc1bf0b78047682
a-rosa <ameliarosalind@my.jcu.edu.au>
2023-04-11 14:07:41 +0800

    Change to have function

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -4,16 +4,24 @@ estimate: 30 mins
 actual: 30 mins
 """
 
-email_details = {}
-email = input("Email: ")
-while email != "":
-    name = " ".join(email.split("@")[0].split(".")).title()
-    correctness = input(f"Is your name {name}? (Y/N) ").upper()
-    if correctness != "" and correctness != "Y":
-        name = input("Name: ").title()
-    email_details[name] = email
+def main():
+    email_details = {}
     email = input("Email: ")
-print()
 
-for detail in email_details.items():
-    print(f"{detail[0]} ({detail[1]})")
+    while email != "":
+        name = get_name(email)
+        correctness = input(f"Is your name {name}? (Y/N) ").upper()
+        if correctness != "" and correctness != "Y":
+            name = input("Name: ").title()
+        email_details[name] = email
+        email = input("Email: ")
+    print()
+
+    for detail in email_details.items():
+        print(f"{detail[0]} ({detail[1]})")
+
+def get_name(email):
+    name = " ".join(email.split("@")[0].split(".")).title()
+    return name
+
+main()
