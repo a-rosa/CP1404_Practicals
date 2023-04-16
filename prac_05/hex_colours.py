cdc8cbd106c782afab08bf602643b4925b1c12bb
a-rosa <ameliarosalind@my.jcu.edu.au>
2023-04-16 13:52:10 +0800

    Edit because the code is wrong

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -2,10 +2,10 @@ COLORS = {"Amber": "#ffbf00", "Beige": "#f5f5dc", "Chocolate": "#d2691e", "Emera
           "Indigo": "#4b0082", "Lavender": "#e6e6fa", "Olive": "808000", "Purple": "#a020f0", "SkyBlue": "#87ceeb"}
 print(COLORS)
 
-picked_color = input("Enter a colour: ").upper()
+picked_color = input("Enter a colour: ").title()
 while picked_color != "":
     if picked_color in COLORS:
         print(COLORS[picked_color])
     else:
         print("Invalid color")
-    picked_color = input("Enter a colour: ").upper()
+    picked_color = input("Enter a colour: ").title()
