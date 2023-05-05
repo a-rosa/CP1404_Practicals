de2084bf88f9bee6aea4dfa7cc005dfabe171f4b
a-rosa <ameliarosalind@my.jcu.edu.au>
2023-05-05 13:47:54 +0800

    Improve storing command

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -5,9 +5,13 @@ def main():
     guitars = []
     in_file = open('guitars.csv', 'r', newline='')
     guitar_data = csv.reader(in_file)
-    for row in guitar_data:
-        guitars.append(row)
+    for name, year, cost in guitar_data:
+        guitars.append(Guitar(name, int(year), float(cost)))
     in_file.close()
+    
+    for guitar in guitars:
+        print(guitar)
+
 
 
 main()
