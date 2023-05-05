f6577fa84df1c1936ce0b036692f60ba259c1e32
a-rosa <ameliarosalind@my.jcu.edu.au>
2023-05-05 13:43:14 +0800

    Add storing function instead of just printing

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -2,10 +2,12 @@ import csv
 from prac_06.guitar import Guitar
 
 def main():
+    guitars = []
     in_file = open('guitars.csv', 'r', newline='')
-    guitars = csv.reader(in_file)
-    for row in guitars:
-        print(row)
+    guitar_data = csv.reader(in_file)
+    for row in guitar_data:
+        guitars.append(row)
     in_file.close()
+
 
 main()
