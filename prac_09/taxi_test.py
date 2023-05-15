fef82cd51c8113c96311fb57eb181ef99701a106
a-rosa <ameliarosalind@my.jcu.edu.au>
2023-05-15 14:41:39 +0800

    Change to also test get fare

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -4,8 +4,10 @@ def main():
     my_taxi = Taxi("Prius 1", 100)
     my_taxi.drive(40)
     print(my_taxi)
-    my_taxi.current_fare_distance = 0
+    print(my_taxi.get_fare())
+    my_taxi.start_fare()
     my_taxi.drive(100)
     print(my_taxi)
+    print(my_taxi.get_fare())
 
 main()
