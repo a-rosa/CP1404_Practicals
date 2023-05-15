35a54e78690d1f90e230eff6bcc2487dabfc494f
a-rosa <ameliarosalind@my.jcu.edu.au>
2023-05-15 14:17:52 +0800

    Test unreliable car class

---
+++ prac_09/unreliable_car_test.py
@@ -0,0 +1,8 @@
+from prac_09.unreliable_car import UnreliableCar
+
+def main():
+    my_unreliable_car = UnreliableCar("Prius 1", 100, 60)
+    my_unreliable_car.drive(40)
+    print(my_unreliable_car)
+
+main()
