5a50d87f9a90e99596d730b50d282ef4b4794888
a-rosa <ameliarosalind@my.jcu.edu.au>
2023-05-15 14:42:10 +0800

    Make silver service taxi class using parent taxi class

---
+++ prac_09/silver_service_taxi.py
@@ -0,0 +1,17 @@
+from taxi import Taxi
+
+class SilverServiceTaxi(Taxi):
+    """A more fancy taxi"""
+    price_per_km = Taxi.price_per_km
+    flagfall = 4.50
+
+    def __init__(self, name, fuel, fanciness):
+        super().__init__(name, fuel)
+        self.price_per_km *= fanciness
+
+    def __str__(self):
+        """Return the total price of the fare"""
+        return f"{super().__str__()} plus flagfall of ${self.flagfall}"
+
+    def get_fare(self):
+        return super().get_fare() + self.flagfall
