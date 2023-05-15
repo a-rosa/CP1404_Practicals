13ca0dc69b0e937ccd01133e9c846151bee83bde
a-rosa <ameliarosalind@my.jcu.edu.au>
2023-05-15 13:41:36 +0800

    Make a test for the taxi

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,11 @@
+from prac_09.taxi import Taxi
+
+def main():
+    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi.drive(40)
+    print(my_taxi)
+    my_taxi.current_fare_distance = 0
+    my_taxi.drive(100)
+    print(my_taxi)
+
+main()
